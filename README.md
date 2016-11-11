663f134228571a945824d42eb1cc041b60ed10a8
Huw <me@huw.nu>
2016-11-12 02:34:27 +1100

    1.1.0
    Added options for including title page in HTML, including HTML content
    in metadata, and overriding metalsmith metadata for `title` and `date`.
    
    Also spaced up code, removed a useless variable, and incremented the
    version number.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # metalsmith-fountain
 
-  A Metalsmith plugin to convert [fountain](http://fountain.io) screenplay files.
+  A Metalsmith plugin to convert [Fountain](http://fountain.io) screenplay files.
 
   This is based on a modified parser from Matt Daly's [Fountain.js](https://github.com/mattdaly/Fountain.js)
 
@@ -9,9 +9,30 @@
 
     $ npm install metalsmith-fountain
 
+## Options
+
+   * `title_page`: Whether to include HTML for the title page in the output file (optional, default: `true`)
+   *  `content_metadata`: Whether to include the HTML for the title page and script in metalsmith metadata (may be large!) (optional, default: `false`)
+   *  `preserve_title`: Whether to override the `title` attribute in the metalsmith metadata (optional, default: `false`)
+   *  `preserve_date`: Whether to override the `date` attribute in the metalsmith metadata. Automatically chooses `date` if available, then `draft_date` (optional, default `false`)
+
 ## Usage
 
-  There are no options for this plugin. However, it will add most fountain metadata, accessible from the `fountain` object.
+  This plugin will add most Fountain metadata, accessible from the `fountain` object in Metalsmith's metadata. YAML front-matter is supported alongside Fountain metadata, like so:
+
+  ```
+  ---
+  title: Big Fish
+  credit: written by
+  author: John August
+  ---
+  Title: Big Fish
+  Credit: written by
+  Author: John August
+  ...
+  ```
+
+  Although any key and value can be used in the front-matter, only the following keys will be read from the Fountain metadata into the `fountain` object like so:
 
   ```
   fountain: {
@@ -25,8 +46,15 @@
     date: 'Date'
     contact: 'Contact Details'
     copyright: 'Copyright'
+    content: {  
+      // Only if enabled with the `content_metadata` flag!
+      title_page: '<h1>Title</h1><etc/>',
+      script: '<p>HTML content of the script</p>'
+    }
   }
   ```
+
+  By default, the `title` and `date` attributes in Metalsmith's metadata will also be overridden with values from the Fountain metadata (where `date` takes priority over `draft_date` if both are present). To disable this, set `preserve_title` and `preserve_date` to false, respectively.
 
 ## License
 
--- index.js
+++ index.js
@@ -9,29 +9,75 @@ module.exports = plugin;
 /**
  * Metalsmith plugin to convert fountain files.
  *
+ * @param {Object} [options]
+ *  @property {Boolean} [title_page=true] - Whether to include HTML for the title page in the output file
+ *  @property {Boolean} [content_metadata=false] - Whether to include the HTML for the title page and script in metalsmith metadata (may be large!)
+ *  @property {Boolean} [preserve_title=false] - Whether to override the `title` attribute in the metalsmith metadata
+ *  @property {Boolean} [preserve_date=false] - Whether to override the `date` attribute in the metalsmith metadata. Automatically chooses `date` if available, then `draft_date`.
  * @return {Function}
  */
 
-function plugin() {
-  return function(files, metalsmith, done){
+function plugin(options) {
+  options = options || {};
+  var title_page = true;
+  var content_metadata = false;
+  var preserve_title = false;
+  var preserve_date = false;
+
+  if (typeof options.title_page !== "undefined") title_page = options.title_page
+  if (typeof options.content_metadata !== "undefined") content_metadata = options.content_metadata
+  if (typeof options.preserve_title !== "undefined") preserve_title = options.preserve_title
+  if (typeof options.preserve_date !== "undefined") preserve_date = options.preserve_date
+
+  return function(files, metalsmith, done) {
     setImmediate(done);
-    Object.keys(files).forEach(function(file){
+
+    Object.keys(files).forEach(function(file) {
       debug('checking file: %s', file);
       if (!fountain(file)) return;
+
       var data = files[file];
       var dir = dirname(file);
       var html = basename(file, extname(file)) + '.html';
+
       if ('.' != dir) html = dir + '/' + html;
 
       debug('converting file: %s', file);
       var stringContents = data.contents.toString();
-      var output = parser.parse(stringContents, function(output) {
-        var titlePage = output.html.title_page;
-        var script = output.html.script;
-        var metadataLength = output.metadata.length;
 
-        data.contents = new Buffer(titlePage + script);
+      var output = parser.parse(stringContents, function(output) {
+        var titlePageHTML = output.html.title_page;
+        var scriptHTML = output.html.script;
+
+        if (title_page) {
+          debug('title_page is true');
+          data.contents = new Buffer(titlePageHTML + scriptHTML);
+        } else {
+          debug('title_page is false');
+          data.contents = new Buffer(scriptHTML);
+        }
+
         data.fountain = output.metadata;
+
+        if (content_metadata) {
+          data.fountain.contents = {
+            title_page: titlePage,
+            script: script
+          };
+        }
+
+        if (!preserve_title && output.metadata.title) {
+          data.title = output.metadata.title;
+        }
+
+        if (!preserve_date) {
+          if (output.metadata.date) {
+            data.date = output.metadata.date;
+          } else if (output.metadata.draft_date) {
+            data.date = output.metadata.draft_date;
+          }
+        }
+
         debug('added metadata: %s', JSON.stringify(data.fountain));
         delete files[file];
         files[html] = data;
